f12499cc0032a2e81b44a90d09e44079f584c96c
王保成 <wangbaocheng@qutoutiao.net>
2021-08-11 10:06:06 +0800

    log in前端模板渲染

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 import configparser
-from flask import Flask
-
+from flask import *
 import type
+from werkzeug.utils import redirect
+
 
 app = Flask(__name__)
 
 
+session = {'logged_in': False}        # 状态存储 str->bool
+
+
 @app.route('/')
-def hello_word():
+def home():
     """
-        网关心跳
+    Home Page
     """
-    print("Hello World")
+    if not session['logged_in']:
+        return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    """
+        main screen
+    """
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    """
+    log out
+    """
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
@@ -20,14 +41,12 @@ if __name__ == '__main__':
     config.read('config.ini')
     if 'port' not in config['SETUP']:
         config['SETUP']['port'] = type.DEFAULT_PORT
-    print("="*70)
-    print("""
-    项目:   Flask项目模板
+    print("="*80)
+    print("""项目:   Flask项目模板
     作者:   王保成
     时间:   2021-08-10 19:16
     IP:     127.0.0.1
-    端口:   {}
-    """.format(config['SETUP']['port']))
-    print("="*70)
+    端口:   {}""".format(config['SETUP']['port']))
+    print("="*80)
     app.run()
 
---
+++ run.command
@@ -0,0 +1,4 @@
+#!/bin/bash
+# MacOS 可执行文件 - 获取当前位置并运行main()所在文件
+cd "`dirname "$0"`"
+python3 app.py
